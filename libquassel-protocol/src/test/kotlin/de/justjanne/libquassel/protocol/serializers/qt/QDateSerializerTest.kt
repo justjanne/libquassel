ae696d81bbde208c7251b030f566bec1d66d1a72
Janne Mareike Koschinski <janne@kuschku.de>
2021-02-28 22:21:08 +0100

    Replace copyright after all third-party sources have been eliminated

--- libquassel-protocol/src/test/kotlin/de/justjanne/libquassel/protocol/serializers/qt/QDateSerializerTest.kt
+++ libquassel-protocol/src/test/kotlin/de/justjanne/libquassel/protocol/serializers/qt/QDateSerializerTest.kt
@@ -1,7 +1,6 @@
 /*
  * libquassel
  * Copyright (c) 2021 Janne Mareike Koschinski
- * Copyright (c) 2021 The Quassel Project
  *
  * This Source Code Form is subject to the terms of the Mozilla Public License,
  * v. 2.0. If a copy of the MPL was not distributed with this file, You can
